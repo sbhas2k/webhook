278c5fe27507164ecf25f99bdd0dc549052d2130
sbhas2k <137640379+sbhas2k@users.noreply.github.com>
2023-06-25 08:59:41 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask,render_template
-
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+SCRIPTS = [{
+  'id': 1,
+  'symbol': 'INFY',
+  'traded_price': '111.11',
+  'ltp': '111.11'
+}, {
+  'id': 2,
+  'symbol': 'TCS',
+  'traded_price': '1011.11',
+  'ltp': '1101.11'
+}, {
+  'id': 3,
+  'symbol': 'HCL',
+  'traded_price': '2123.11',
+  'ltp': '2123'
+}]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template(
+    'home.html',
+    scripts=SCRIPTS,
+  )
 
 
-if __name__ =="__main__":
+@app.route("/api/scripts")
+def list_scripts():
+  return jsonify(SCRIPTS)
+
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
