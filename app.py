76535d19fac0221200ec3db5db3336d2115b5242
sbhas2k <137640379+sbhas2k@users.noreply.github.com>
2023-07-09 04:35:43 +0000

    updated code

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
 from flask import Flask, render_template, jsonify
-from database import load_scripts_from_db
+from database import load_scripts_from_db, load_script_from_db
 # from sqlalchemy import text
 
 app = Flask(__name__)
-
-# SCRIPTS = [{
-#   'id': 1,
-#   'symbol': 'INFY',
-#   'traded_price': '111.11',
-#   'ltp': '111.11'
-# }, {
-#   'id': 2,
-#   'symbol': 'TCS',
-#   'traded_price': '1011.11',
-#   'ltp': '1101.11'
-# }, {
-#   'id': 3,
-#   'symbol': 'HCL',
-#   'traded_price': '2123.11',
-#   'ltp': '2123'
-# }]
 
 
 @app.route("/")
@@ -35,6 +18,21 @@ def hello_world():
 def list_scripts():
   SCRIPTS = load_scripts_from_db()
   return jsonify(SCRIPTS)
+
+
+@app.route("/api/script/<id>")
+def show_script(id):
+  script = load_script_from_db(id)
+  return jsonify(script)
+  # return render_template('script.html', script=script)
+
+
+@app.route("/script/<id>")
+def show_script_id(id):
+  script = load_script_from_db(id)
+  if not script:
+    return "Not Found", 404
+  return render_template('script.html', script=script)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -8,18 +8,6 @@ engine = create_engine(conn_string,
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
-# with engine.connect() as con:
-#   result = con.execute(
-#     text(
-#       "SELECT id,symbol,tp,ltp,pos_status,profit_loss,(tp-ltp) as pnl from stock_list"
-#     ))
-#   result_dict = []
-#   for row in result.all():
-#     result_dict.append(row._asdict())
-
-#   print(result_dict)
-#   print(type(result_dict))
-
 
 def load_scripts_from_db():
   with engine.connect() as con:
@@ -32,3 +20,20 @@ def load_scripts_from_db():
     SCRIPTS.append(row._asdict())
 
   return SCRIPTS
+
+
+def load_script_from_db(id):
+  with engine.connect() as con:
+    result = con.execute(text("SELECT * from stock_list where id = :val"),
+                         {"val": id})
+
+  rows = result.all()
+  if len(rows) == 0:
+    return None
+  else:
+    return rows[0]._asdict()
+  # SCRIPTS = []
+  # for row in result.all():
+  #   SCRIPTS.append(row._asdict())
+
+  # return SCRIPTS
